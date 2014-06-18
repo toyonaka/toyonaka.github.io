c35739496ebf4c6913e7b8b80132ada45f47ca84
toyonaka <tomimototomimoto@gmail.com>
2014-06-18 20:08:09 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -10,8 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'yokohama_budget';
-OpenSpending.year = '2012';
+OpenSpending.identifier = 'toyonaka_budget';
+OpenSpending.year = '2014';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
