75f747938d8f3cc4c59bc244634882d55142d12f
toyonaka <tomimototomimoto@gmail.com>
2014-06-19 11:03:43 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -19,59 +19,59 @@ OpenSpending.Styles.Cofog = {
 
   /* toyonaka cofog */
   '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '1-101': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '101': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
   '2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-201': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-202': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-203': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-204': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-205': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-206': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
+  '201': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
+  '202': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
+  '203': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
+  '204': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
+  '205': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
+  '206': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
   '3': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-301': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-302': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-303': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-304': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-305': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-306': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-307': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-308': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-309': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '301': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '302': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '303': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '304': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '305': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '306': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '307': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '308': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '309': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
   '4': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#D33673' },
-  '4-401': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#D33673' },
-  '4-402': { icon: 'icons/street-lights.svg', color: '#C75746', bcolor: '#D33673' },
+  '401': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#D33673' },
+  '402': { icon: 'icons/street-lights.svg', color: '#C75746', bcolor: '#D33673' },
   '5': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
-  '5-501': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
+  '501': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
   '6': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
-  '6-601': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
+  '601': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
   '7': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
-  '7-701': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
+  '701': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
   '8': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-801': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-802': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-803': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-804': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-805': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-806': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-807': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
+  '801': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
+  '802': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
+  '803': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
+  '804': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
+  '805': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
+  '806': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
+  '807': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
   '9': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
-  '9-901': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
+  '901': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
   '10': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
-  '10-1001': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
-  '10-1002': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
-  '10-1003': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
-  '10-1004': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
-  '10-1005': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
-  '10-1006': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1001': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1002': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1003': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1004': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1005': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1006': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
   '11': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
-  '11-1101': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1101': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
   '12': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
-  '12-1201': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
-  '12-1202': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
-  '12-1203': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
-  '12-1204': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
-  '12-1205': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
-  '12-1206': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1201': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1202': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1203': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1204': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1205': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1206': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
   '13': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
 };
 
